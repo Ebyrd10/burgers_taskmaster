ca6cac3071b3d76634af8a705c6c20518d555b01
Ethan <Ethanmbyrd@gmail.com>
2020-05-13 09:54:12 -0400

    adding schema and seeding

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger) VALUES (`The King Burger`);
+INSERT INTO burgers (burger) VALUES (`Bacon Burger`);
+INSERT INTO burgers (burger) VALUES (`Bramble Burger`);
+INSERT INTO burgers (burger) VALUES (`Burger Topped with Egg`);
